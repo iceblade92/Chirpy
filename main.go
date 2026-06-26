8fa0ed0e8565c732f57ed60703cfd49e031391f4
iceblade92 <163899714+iceblade92@users.noreply.github.com>
2026-06-26 19:30:28 +0200

    auth and login and big refactor

--- main.go
+++ main.go
@@ -53,6 +53,7 @@ func main() {
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirp)
 	mux.HandleFunc("GET /api/chirps", apiCfg.handlerGetChirps)
 	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerGetChirp)
+	mux.HandleFunc("POST /api/login", apiCfg.handlerLogin)
 
 	server := &http.Server{
 		Addr:    ":8080",
